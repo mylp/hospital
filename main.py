16c040c084c1e0ca5daec2f2a5db8e9eaffd2008
mhpei <mhpei@uiowa.edu>
2023-02-14 16:16:37 -0600

    Basic DB Connection

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
+import mysql.connector
+import unittest
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="PepeSilvia1259#12!",
+    database="test"
+)
+
+cursor = mydb.cursor()
+mydb.autocommit = True
+
 def main():
-    print("Hello World!")
+    cursor.callproc('get_hospitals')
+    for result in cursor.stored_results():
+        print(result.fetchall())
+    
 
 if __name__ == '__main__':
     main()
