9aea1349ce3a649fc9a4f57b62da83a6d51dfc15
mhpei <mhpei@uiowa.edu>
2023-02-14 16:41:10 -0600

    DB

--- hospital_db.sql
+++ hospital_db.sql
@@ -1,1 +1,48 @@
 ---------Enter the MySql script here-----
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema test
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema test
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `test` ;
+
+-- -----------------------------------------------------
+-- Table `test`.`hospital`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test`.`hospital` (
+  `idhospital` INT NOT NULL,
+  PRIMARY KEY (`idhospital`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+USE `test` ;
+
+-- -----------------------------------------------------
+-- procedure get_hospitals
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `test`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_hospitals`()
+BEGIN
+	SELECT * FROM hospital;
+END$$
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
