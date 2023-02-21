d66f1ca7dd1065f71ff4b8a08b8164575e555f69
RupB <rupanti-bose@uiowa.edu>
2023-02-21 14:40:45 -0600

    create user and user log in procedure added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="PepeSilvia1259#12!",
+    password="root3069",
     database="test"
 )
 
