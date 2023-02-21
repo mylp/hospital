6c9b62f51b9c74d10fa5d046008473db3540de5f
Miles Pei <mpei36@gmail.com>
2023-02-21 11:30:13 -0600

    DB Update

--- hospital_db.sql
+++ hospital_db.sql
@@ -1,4 +1,3 @@
----------Enter the MySql script here-----
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -19,11 +18,75 @@ CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4
 USE `test` ;
 
 -- -----------------------------------------------------
--- Table `test`.`hospital`
+-- Table `test`.`appointment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test`.`hospital` (
-  `idhospital` INT NOT NULL,
-  PRIMARY KEY (`idhospital`))
+CREATE TABLE IF NOT EXISTS `test`.`appointment` (
+  `idappointment` INT NOT NULL AUTO_INCREMENT,
+  `appointment_date` DATETIME NOT NULL,
+  `idpatient` INT NOT NULL,
+  `idphysician` INT NOT NULL,
+  `description` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`idappointment`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `test`.`nurse`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test`.`nurse` (
+  `idnurse` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `iduser` INT NOT NULL,
+  PRIMARY KEY (`idnurse`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `test`.`patient`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test`.`patient` (
+  `idpatient` INT NOT NULL,
+  `idnurse` INT NULL DEFAULT NULL,
+  `idphysician` INT NOT NULL,
+  `iduser` INT NOT NULL,
+  PRIMARY KEY (`idpatient`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `test`.`physician`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test`.`physician` (
+  `idphysician` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `iduser` INT NOT NULL,
+  PRIMARY KEY (`idphysician`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `test`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test`.`user` (
+  `iduser` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(15) NOT NULL,
+  `password` VARCHAR(15) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `phone_no` VARCHAR(15) NOT NULL,
+  `status` INT NOT NULL COMMENT '0 - Patient\\n1 - Nurse\\n2 - Physician\\n3 - Admin',
+  PRIMARY KEY (`iduser`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
