239b4868ce2ae80b7155cd4e5997dd560a988d05
mhpei <mhpei@uiowa.edu>
2023-03-31 16:14:52 -0500

    changes

--- unittest/test_login.py
+++ unittest/test_login.py
@@ -14,42 +14,13 @@ class TestSignUp(TestCase):
     def create_app(self):
         return app
 
-    def test_signup_with_required_fields(self):
-        response = self.client.post('/api/signup', data=dict(
-            inputFirst='John',
-            inputLast='Doe',
-            inputStreet='123 Main St',
-            inputCity='Anytown',
-            inputState='CA',
-            inputZip='12345',
-            inputPhone='555-123-4567',
-            inputDOB='01/01/2000',
-            inputSex='Male',
-            inputEmail='john.doe@example.com',
+    def test_login_with_correct_credentials(self):
+        response = self.client.post('/api/validateLogin', data=dict(
             inputUsername='johndoe',
-            inputPassword='password'
+            inputPassword='password',
         ))
         self.assertEquals(response.location, '/userHome')
         self.assertEquals(response.status_code, 302)
-    
-    def test_signup_with_missing_field(self):
-        response = self.client.post('/api/signup', data=dict(
-            inputFirst='John',
-            inputLast='Doe',
-            inputStreet='123 Main St',
-            inputCity='Anytown',
-            inputState='',
-            inputZip='12345',
-            inputPhone='555-123-4567',
-            inputDOB='01/01/2000',
-            inputSex='Male',
-            inputEmail='john.doe@example.com',
-            inputUsername='johndoe',
-            inputPassword='password'
-        ))
-        self.assertEquals(response.location, None)
-        self.assertEquals(response.status_code, 200)
-
 
 
 if __name__ == '__main__':
