c1f71120f0e5b36d04516612bff53d331af512d3
mhpei <mhpei@uiowa.edu>
2023-03-31 16:03:52 -0500

    Test redirects working

--- unittest/test_signup.py
+++ unittest/test_signup.py
@@ -1,17 +1,21 @@
 import unittest
 import os
 import sys
+from flask import url_for
+from flask_testing import TestCase
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
-class TestSignUp(unittest.TestCase):
 
-    def setUp(self):
-        self.app = app.test_client()
+
+class TestSignUp(TestCase):
+
+    def create_app(self):
+        return app
 
     def test_signup_with_required_fields(self):
-        response = self.app.post('/api/signup', data=dict(
+        response = self.client.post('/api/signup', data=dict(
             inputFirst='John',
             inputLast='Doe',
             inputStreet='123 Main St',
@@ -25,15 +29,16 @@ class TestSignUp(unittest.TestCase):
             inputUsername='johndoe',
             inputPassword='password'
         ))
-        self.assertEqual(response.status_code, 302)  # redirect to /userHome
-
-    def test_signup_with_missing_fields(self):
-        response = self.app.post('/api/signup', data=dict(
+        self.assertEquals(response.location, '/userHome')
+        self.assertEquals(response.status_code, 302)
+    
+    def test_signup_with_missing_field(self):
+        response = self.client.post('/api/signup', data=dict(
             inputFirst='John',
             inputLast='Doe',
-            inputStreet='',
+            inputStreet='123 Main St',
             inputCity='Anytown',
-            inputState='CA',
+            inputState='',
             inputZip='12345',
             inputPhone='555-123-4567',
             inputDOB='01/01/2000',
@@ -42,7 +47,10 @@ class TestSignUp(unittest.TestCase):
             inputUsername='johndoe',
             inputPassword='password'
         ))
-        self.assertEqual(response.status_code, 200)  # return error message
+        self.assertEquals(response.location, None)
+        self.assertEquals(response.status_code, 200)
+
+
 
 if __name__ == '__main__':
     unittest.main()
