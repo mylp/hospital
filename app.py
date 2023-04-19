befb719a21c11c43408a7aea720de1c6ca87d9dc
Miles Pei <mpei36@gmail.com>
2023-04-19 18:09:32 -0500

    Added billing and statement display pages

--- app.py
+++ app.py
@@ -245,6 +245,17 @@ def showBilling():
     return render_template('billing.html', unpaid=unpaid, paid=paid)
 
 
+@app.route('/showStatement', methods=['GET','POST'])
+def showStatement():
+    statement_id = request.args.get(('statement_id'))
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.callproc('sp_getInvoices', (statement_id,))
+    invoices = cursor.fetchall()
+    return render_template('statement.html', invoices=invoices)
+
+
+
 @app.route('/appointment')
 def showAppointment():
     user_appointments = getAppointments()
